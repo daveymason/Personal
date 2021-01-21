47c635e143bef2c89f8d4a8eed88565385304aa0
Dave <daveymason@outlook.com>
2021-01-21 17:13:51 +0800

    Edited Contact page and footer
    Added missing CV links, removed form from every page and finished contact page

--- footer.php
+++ footer.php
@@ -1,39 +1,22 @@
-  <div class="container" id="Contact">
-      <div class="row bottomMargin">
-        <div class="col-sm-12 text-center servicesPadding">
-          <h2 class="display-3"><i class="fas fa-comment-dots">
-          </i>
-          Contact</h2>
-          <p>Interested in my services?</p>
+  <div class="container" id="contactFooter">
+      <div class="row extraPadding">
+        <div class="col-md-4 centerAll">
+          <h2 class="display-3"><i class="fas fa-comment-dots"></i><br>Contact</h2>
         </div>
-      </div>
-      <div class="row">
-        <div class="col-md-6 col-sm-12 centerAll bottomMargin">
-          <div class="card text-center">
-            <div class="card-header cardHeadGreen">
-              <h2>E-mail Me</h2>
-            </div>
-            <div class="card-body">
+     
+        <div class="col-md-4 centerAll">
              <button type="button" class="btn hvr-float-shadow"><a href="mailto:daveymason@outlook.com" target="_blank"><i class="far fa-envelope bigStyleContact ServIconShake1"></i></a></button>
-
-              <p class="textPadding">For <b>website design</b>, <b>website development</b>, <b>SEO</b>, gaming services, <b>online teaching</b>, domains, <b>website hosting</b>, <b>Wordpress</b> websites, design and development; or any other web services that you might need feel free to e-mail me by clicking on the icon above or directly - <a href="mailto:daveymason@outlook.com" target="_blank">daveymason@outlook.com</a></p>
-              <p><a class="btn btn-contact btn-lg" href="mailto:daveymason@outlook.com" role="button">E-mail now <i class="far fa-arrow-alt-circle-right btnRightArrow"></i></a></p>
-            </div>
-          </div>
+            <p><a class="btn btn-contact btn-lg" href="mailto:daveymason@outlook.com" role="button">Quick E-mail <i class="far fa-arrow-alt-circle-right btnRightArrow"></i></a></p>
         </div>
         
-         <div class="col-md-6 col-sm-12">
-          <div class="card bottomMargin centerAll">
-            <div class="card-header cardHeadGreen">
-              <h2>Online Query</h2>
-            </div>
-            <div class="card-body">
-              <?php  echo do_shortcode( '[forminator_form id="124"]' ); ?> 
-            </div>
-          </div>  
+         <div class="col-md-4 centerAll">
+             <button type="button" class="btn hvr-float-shadow"><a href="mailto:daveymason@outlook.com" target="_blank"><i class="fas fa-columns bigStyleContact ServIconShake4"></i></a></button>
+            <p><a class="btn btn-contact btn-lg" href="/contact" role="button">Contact Page <i class="far fa-arrow-alt-circle-right btnRightArrow"></i></a></p>  
         </div>  
       </div>
-    </div>
+</div>
+
+    <hr class="contactSplit">
 
 <footer>
       <div class="container">
@@ -55,15 +38,15 @@
             <ul class="list-unstyled social-list">
               <li class="hvr-float-shadow"><a href="https://www.linkedin.com/in/david-mason-1623277b/" target="_blank"><i class="fab fa-linkedin-in footerStyleLi"></i></a></li>
               <li class="hvr-float-shadow"><a href="https://www.github.com/davidmason5" target="_blank"><i class="fab fa-github footerStyleGh"></i></a></li>
-              <li class="hvr-float-shadow"><a href="#" target="_blank"><i class="fas fa-id-card footerStyleCV"></i></a></li>
+              <li class="hvr-float-shadow"><a href="https://daveymason.com/wp-content/uploads/2021/01/David-Mason-CV.pdf" target="_blank"><i class="fas fa-id-card footerStyleCV"></i></a></li>
               <li class="hvr-float-shadow"><a href="mailto:daveymason@outlook.com" target="_blank"><i class="far fa-envelope footerStyleEmail"></i></a></li>
             </ul>
           </div>
           
           <div class="col-lg-4 col-md-6">
-            <h3><span style="color: violet;">Favourite</span><span style="color: blue;"> Articles</span></h3>
+            <h3><span style="color: violet;">Favourite</span><span style="color: blue;"> Articles</span><span class="badge badge-primary">Soon</span></h3>
             <div class="media">
-              <a href="https://daveymason.com/blog/development" class="pull-left">
+              <a href="#" class="pull-left">
                 <img src="https://daveymason.com/wp-content/themes/Personal/Images/blogThumbDev.jpg" alt="" class="media-object hvr-float-shadow" />
               </a>
               <div class="media-body">
--- header.php
+++ header.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.css">  
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/fontawesome.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.css">  
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">  
 
     <title>DaveyMason.com</title>
